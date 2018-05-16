150b1c071c09c1926eaf7ecd0ea0981455ee79c6
Fatima-Barbosa <mariadefatimababosa@outlook.com>
2018-05-15 22:34:45 -0300

    Criando telas de ADM para cadastro de Operadores

---
+++ src/application7/Tela_cad_operadores_Controller.java
@@ -0,0 +1,21 @@
+package application7;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Fátima
+ */
+public class Tela_cad_operadores_Controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        
+        
+    }    
+    
+}
